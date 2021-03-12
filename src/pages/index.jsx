40e195eb76d3e7a59d43ce2ddb382cee407da6a1
Harysson Soares <haryssonsoares@gmail.com>
2021-03-12 07:00:40 -0300

    feat: step wizard for login and home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,30 +1,19 @@
-import React from "react";
-import { Image, Button } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import StepWizard from "react-step-wizard";
 
-import bgContet from "../assets/images/arte-wave.svg";
-import logo from "../assets/images/handonkey.svg";
+import Home from "../components/Home";
+import Login from "../components/Login";
 
-const Home = () => {
+const App = () => {
+  const stepWizard = useRef();
+  const [kindUser, setKindUser] = useState("");
+
   return (
-    <div className="home d-flex justify-content-center align-items-center">
-      <div className="home-content d-flex flex-wrap flex-column p-4">
-        <div className="home-content-logo w-100 d-flex justify-content-center flex-wrap align-items-center">
-          <Image src={logo} className="content-logo-image w-100" />
-          <h1 className="content-logo-title">
-            CAF - Controle de Ambientes Fisicos
-          </h1>
-        </div>
-        <div className="home-content-options w-100 d-flex justify-content-around align-items-center flex-grow-1 flex-wrap">
-          <Button>Funcionário</Button>
-          <Button>Visitante</Button>
-        </div>
-      </div>
-      <div
-        className="home-bg"
-        style={{ backgroundImage: `url(${bgContet})` }}
-      ></div>
-    </div>
+    <StepWizard ref={stepWizard}>
+      <Home setKindUser={setKindUser} />
+      <Login kindUser={kindUser} />
+    </StepWizard>
   );
 };
 
-export default Home;
+export default App;
