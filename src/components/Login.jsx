25ccdae868231f179f8e309f8a3effbbaa50298a
Harysson Soares <haryssonsoares@gmail.com>
2021-03-13 12:47:25 -0300

    feeat: login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { Image, Button, Form } from "react-bootstrap";
+import Link from "next/link";
+import { Button, Form, Image } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 import Input from "../components/Form/Input";
 
+import translate from "../assets/scripts/util/translate";
+
 import bgContet from "../assets/images/arte-wave.svg";
 import logo from "../assets/images/handonkey.svg";
+import user from "../assets/images/icons/user.svg";
+import password from "../assets/images/icons/password.svg";
 
 const Login = ({ nextStep, firstStep, kindUser = "" }) => {
   const { register, handleSubmit, watch, errors } = useForm();
@@ -17,34 +22,57 @@ const Login = ({ nextStep, firstStep, kindUser = "" }) => {
         className="login-bg"
         style={{ backgroundImage: `url(${bgContet})` }}
       ></div>
-      <div className="login-form w-50 h-100">
+      <div className="login-form w-50 h-100 d-flex flex-column align-items-center justify-content-center">
+        <div className="login-form-logo d-flex flex-wrap flex-column align-items-center w-100">
+          <Image src={logo} />
+          <p>
+            Faça seu login como{" "}
+            <strong className="text-uppercase">{translate(kindUser)}</strong>
+          </p>
+        </div>
         <Form
           onSubmit={handleSubmit(onSubmit)}
-          className="w-100 h-100 d-flex flex-column align-items-center flex-wrap"
+          className="w-100 d-flex flex-column align-items-center flex-wrap"
         >
           <Input
-            className="mt-3 text-center login-form-input input-username"
             name="username"
             placeholder="Usuário ou email"
-            insideLabel={() => <i class="fas fa-user"></i>}
+            label={<Image className="mb-1" src={user} />}
+            contextClassName="position-relative mt-4 d-flex justify-content-center"
+            labelClassName="position-absolute login-form-label"
+            className="text-center login-form-input input-username"
           />
+
           <Input
-            className="mt-3 text-center login-form-input input-password"
             name="password"
             type="password"
             placeholder="Senha"
+            label={<Image className="mb-1" src={password} />}
+            contextClassName="position-relative mt-4 d-flex justify-content-center"
+            labelClassName="position-absolute login-form-label"
+            className="text-center login-form-input input-password"
           />
-          <Button className="mt-3 w-50 badge badge-pill" type="submit">
+
+          <Button
+            className="login-form-button mt-3 badge badge-pill"
+            type="submit"
+          >
             Login
           </Button>
 
           <Button
             variant="danger"
             onClick={() => firstStep()}
-            className="mt-3 w-50 badge badge-pill"
+            className="login-form-button  mt-3 badge badge-pill"
           >
             Voltar
           </Button>
+
+          <Link href="/">
+            <a className="mt-3 text-center text-primary login-form-link">
+              Não possui conta? <br /> Clique aqui para cadastre-se
+            </a>
+          </Link>
         </Form>
       </div>
     </div>
