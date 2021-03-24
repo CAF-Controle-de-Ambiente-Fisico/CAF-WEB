d6769ecef65b2c8aac1356c4e505eed283af7652
Harysson Soares <haryssonsoares@gmail.com>
2021-03-24 12:56:35 -0300

    improvements: context to signup visitant

--- src/pages/signup/visitant/index.jsx
+++ src/pages/signup/visitant/index.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useForm } from "react-hook-form";
-import { Button, Form, Image } from "react-bootstrap";
+import { useForm, FormProvider } from "react-hook-form";
+import { Button, Form, Image, Alert } from "react-bootstrap";
 import Dropzone from "react-dropzone";
 
 import Input from "../../../components/Form/Input";
@@ -15,126 +15,155 @@ import avatar from "../../../assets/images/avatar.svg";
 
 const Singup = () => {
   const [image, setImage] = useState();
+  const [sendEmail, setSendEmail] = useState();
 
-  const { register, handleSubmit, watch, errors, setValue } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const methods = useForm();
+  const onSubmit = (data) => {
+    setSendEmail(data.email);
+    console.log(data);
+  };
   const router = useRouter();
 
   const user = router.pathname.split("/")[2];
 
   useEffect(() => {
-    register({ name: "avatar" });
-    setValue("avatar", image);
+    methods.register({ name: "avatar" });
+    methods.setValue("avatar", image);
     return () => {
       URL.revokeObjectURL(image?.preview);
     };
   }, [image]);
 
   return (
-    <div className="signup d-flex">
-      <div
-        className="signup-bg"
-        style={{ backgroundImage: `url(${bgContet})` }}
-      >
-        <Image src={logo} className="ml-5" />
-      </div>
-      <div className="signup-form w-50 h-100 d-flex flex-column align-items-center justify-content-center">
-        <div className="signup-form-title d-flex flex-wrap flex-column align-items-center w-100">
-          <span className="text-primary text-capitalize">
-            Cadastro de {translate(user)}
-          </span>
+    <>
+      {sendEmail ? (
+        <div className="confirmation d-flex justify-content-center align-items-center bg-info vh-100">
+          <div className="confirmation-content d-flex flex-wrap flex-column p-4">
+            <div className="confirmation-content-logo w-100 d-flex justify-content-center flex-wrap align-items-center">
+              <Image src={logo} className="content-logo-image w-100" />
+            </div>
+            <div className="confirmation-content-options w-100 d-flex justify-content-around align-items-center flex-grow-1 flex-wrap">
+              <Alert variant="info">
+                <Alert.Heading className="text-center">
+                  Estamos quase lá!
+                </Alert.Heading>
+                <p className="text-center">Enviamos um email para você.</p>
+                <p className="text-center">
+                  Verifique sua caixa de email com o link de confirmação!
+                </p>
+                <Alert
+                  className="d-flex justify-content-center"
+                  variant="primary"
+                >
+                  <strong className="text-primary">{sendEmail}</strong>
+                </Alert>
+              </Alert>
+            </div>
+          </div>
+          <div
+            className="confirmation-bg"
+            style={{ backgroundImage: `url(${bgContet})` }}
+          ></div>
         </div>
-        <Form
-          onSubmit={handleSubmit(onSubmit)}
-          className="w-100 d-flex flex-column align-items-center flex-wrap"
-        >
-          <Input
-            name="username"
-            placeholder="Nome de usuário"
-            contextClassName="position-relative mt-4 d-flex justify-content-center"
-            labelClassName="position-absolute signup-form-label"
-            className="signup-form-input input-username"
-          />
-
-          <Input
-            name="cpf"
-            type="text"
-            placeholder="CPF"
-            contextClassName="position-relative mt-4 d-flex justify-content-center"
-            labelClassName="position-absolute signup-form-label"
-            className="signup-form-input input-password"
-          />
-
-          <Input
-            name="email"
-            type="email"
-            placeholder="Digite seu melhor email"
-            contextClassName="position-relative mt-4 d-flex justify-content-center"
-            labelClassName="position-absolute signup-form-label"
-            className="signup-form-input input-username"
-          />
-
-          <Input
-            name="password"
-            type="password"
-            placeholder="Senha"
-            contextClassName="position-relative mt-4 d-flex justify-content-center"
-            labelClassName="position-absolute signup-form-label"
-            className="signup-form-input input-password"
-          />
-
-          <Input
-            name="password_confirmation"
-            type="password_confirmation"
-            placeholder="Digite sua senha novamente"
-            contextClassName="position-relative mt-4 d-flex justify-content-center"
-            labelClassName="position-absolute signup-form-label"
-            className="signup-form-input input-password"
-          />
-
-          <Dropzone
-            onDrop={(acceptedFiles) =>
-              setImage(
-                Object.assign(acceptedFiles[0], {
-                  preview: URL.createObjectURL(acceptedFiles[0]),
-                })
-              )
-            }
+      ) : (
+        <div className="signup d-flex">
+          <div
+            className="signup-bg"
+            style={{ backgroundImage: `url(${bgContet})` }}
           >
-            {({ getRootProps, getInputProps }) => (
-              <div
-                {...getRootProps()}
-                className={`mt-4 signup-form-dropzone ${
-                  image && "rounded-circle border-0"
-                }`}
+            <Image src={logo} className="ml-5" />
+          </div>
+          <div className="signup-form w-50 h-100 d-flex flex-column align-items-center justify-content-center">
+            <div className="signup-form-title d-flex flex-wrap flex-column align-items-center w-100">
+              <span className="text-primary text-capitalize">
+                Cadastro de {translate(user)}
+              </span>
+            </div>
+            <FormProvider {...methods}>
+              <Form
+                onSubmit={methods.handleSubmit(onSubmit)}
+                className="w-100 d-flex flex-column align-items-center flex-wrap"
               >
-                <input {...getInputProps({ multiple: false })} name="avatar" />
-                <div
-                  className="d-flex form-dropzone-avatar justify-content-center align-items-center h-100 w-100"
-                  style={{
-                    backgroundImage: `url(${image ? image.preview : avatar})`,
-                  }}
-                ></div>
-              </div>
-            )}
-          </Dropzone>
+                <Input
+                  required
+                  name="username"
+                  placeholder="Nome de usuário"
+                  contextClassName="position-relative mt-4 d-flex justify-content-center"
+                  labelClassName="position-absolute signup-form-label"
+                  className="signup-form-input input-username"
+                />
 
-          <Button
-            variant="warning"
-            className="signup-form-button mt-3 badge badge-pill text-white"
-            type="submit"
-          >
-            Inscrever-se
-          </Button>
+                <Input
+                  required
+                  name="cpf"
+                  type="text"
+                  placeholder="CPF"
+                  contextClassName="position-relative mt-4 d-flex justify-content-center"
+                  labelClassName="position-absolute signup-form-label"
+                  className="signup-form-input input-password"
+                />
 
-          <Link href="/">
-            <a className="mt-3 text-center text-primary signup-form-link">
-              Voltar
-            </a>
-          </Link>
-        </Form>
-      </div>
-    </div>
+                <Input
+                  name="email"
+                  type="email"
+                  required
+                  placeholder="Digite seu melhor email"
+                  contextClassName="position-relative mt-4 d-flex justify-content-center"
+                  labelClassName="position-absolute signup-form-label"
+                  className="signup-form-input input-username"
+                />
+
+                <Dropzone
+                  onDrop={(acceptedFiles) =>
+                    setImage(
+                      Object.assign(acceptedFiles[0], {
+                        preview: URL.createObjectURL(acceptedFiles[0]),
+                      })
+                    )
+                  }
+                >
+                  {({ getRootProps, getInputProps }) => (
+                    <div
+                      {...getRootProps()}
+                      className={`mt-4 signup-form-dropzone ${
+                        image && "rounded-circle border-0"
+                      }`}
+                    >
+                      <input
+                        {...getInputProps({ multiple: false })}
+                        name="avatar"
+                      />
+                      <div
+                        className="d-flex form-dropzone-avatar justify-content-center align-items-center h-100 w-100"
+                        style={{
+                          backgroundImage: `url(${
+                            image ? image.preview : avatar
+                          })`,
+                        }}
+                      ></div>
+                    </div>
+                  )}
+                </Dropzone>
+
+                <Button
+                  variant="warning"
+                  className="signup-form-button mt-4 badge badge-pill text-white"
+                  type="submit"
+                >
+                  Inscrever-se
+                </Button>
+
+                <Link href="/">
+                  <a className="mt-4 text-center text-primary signup-form-link">
+                    Voltar
+                  </a>
+                </Link>
+              </Form>
+            </FormProvider>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
