179ed338746bf5a9ba0d6a090b01fc76c52c6f67
Harysson Soares <haryssonsoares@gmail.com>
2021-03-24 14:15:31 -0300

    feat: home responsive page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,13 +8,14 @@ const Home = ({ nextStep, setKindUser }) => {
   return (
     <div className="home d-flex justify-content-center align-items-center bg-info">
       <div className="home-content d-flex flex-wrap flex-column p-4">
-        <div className="home-content-logo w-100 d-flex justify-content-center flex-wrap align-items-center">
-          <Image src={logo} className="content-logo-image w-100" />
-          <h1 className="content-logo-title">
-            CAF - Controle de Ambientes Fisicos
-          </h1>
-        </div>
-        <div className="home-content-options w-100 d-flex justify-content-around align-items-center flex-grow-1 flex-wrap">
+        <div
+          className="home-content-logo w-100 d-flex justify-content-center flex-wrap align-items-center"
+          style={{ backgroundImage: `url(${logo})` }}
+        ></div>
+        <h1 className="content-logo-title text-primary">
+          Controle de Ambientes Fisicos
+        </h1>
+        <div className="home-content-options w-100 d-flex flex-grow-1 flex-wrap">
           <Button
             onClick={() => {
               setKindUser("employee");
