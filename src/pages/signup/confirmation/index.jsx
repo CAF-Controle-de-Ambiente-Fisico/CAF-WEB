40b19d0e72544b1b20c33d9d0151813f955f4664
Harysson Soares <haryssonsoares@gmail.com>
2021-04-01 01:11:12 -0300

    refact: confirmation page

--- src/pages/signup/confirmation/index.jsx
+++ src/pages/signup/confirmation/index.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import { Image, Form, Button } from "react-bootstrap";
-import { useForm, FormProvider } from "react-hook-form";
+import { Image, Button, Alert } from "react-bootstrap";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 
 import bgContet from "../../../assets/images/arte-wave.svg";
 import logo from "../../../assets/images/handonkey.svg";
-import Input from "../../../components/Form/Input";
 
 const Confirmation = () => {
   const MySwal = withReactContent(Swal);
@@ -32,49 +31,26 @@ const Confirmation = () => {
     }
   };
 
-  const methods = useForm();
-
   return (
     <div className="confirmation d-flex justify-content-center align-items-center bg-info vh-100">
       <div className="confirmation-content d-flex flex-wrap flex-column p-4">
         <div className="confirmation-content-logo w-100 d-flex justify-content-center flex-wrap align-items-center">
           <Image src={logo} className="content-logo-image w-100" />
-          <p>Cadastre sua senha</p>
         </div>
-        <div className="confirmation-content-form w-100 d-flex justify-content-around align-items-center flex-grow-1 flex-wrap">
-          <FormProvider {...methods}>
-            <Form
-              onSubmit={methods.handleSubmit(onSubmit)}
-              className="w-100 d-flex flex-column align-items-center flex-wrap"
-            >
-              <Input
-                required
-                name="password"
-                type="password"
-                placeholder="Senha"
-                contextClassName="position-relative mt-3 d-flex justify-content-center"
-                labelClassName="position-absolute confirmation-form-label"
-                className="confirmation-form-input input-password"
-              />
-
-              <Input
-                required
-                name="passwordConfirmation"
-                type="password"
-                placeholder="Digite sua senha novamente"
-                contextClassName="position-relative mt-3 d-flex justify-content-center"
-                labelClassName="position-absolute confirmation-form-label"
-                className="confirmation-form-input input-password"
-              />
-
-              <Button
-                className="login-form-button mt-3 badge badge-pill"
-                type="submit"
-              >
-                Salvar
-              </Button>
-            </Form>
-          </FormProvider>
+        <div className="confirmation-content-options w-100 d-flex justify-content-around mt-5 flex-wrap">
+          <Alert variant="success" className="w-50 p-4">
+            <Alert.Heading className="text-center">
+              Bem vindo <br />{" "}
+              <strong className="text-uppercase"> {"fulano de tal"}</strong>.
+            </Alert.Heading>
+            <hr />
+            <p className="text-center">Faça login para acessar sua conta!</p>
+            <div className="d-flex justify-content-center">
+              <Link href="/">
+                <a className="btn btn-success mt-3 text-center">Acessar CAF</a>
+              </Link>
+            </div>
+          </Alert>
         </div>
       </div>
       <div
