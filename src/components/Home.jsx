77779a449a666c84fa429c69da0533244ffa74d4
edubcoelho <luizbcoelho27@outlook.com>
2021-04-06 12:02:40 -0300

    home styled

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import logo from "../assets/images/handonkey.svg";
 const Home = ({ nextStep, setKindUser }) => {
   return (
     <div className="home d-flex justify-content-center align-items-center bg-info">
-      <div className="home-content d-flex flex-wrap flex-column p-4">
+      <div className="home-content d-flex flex-column p-5">
         <div
           className="home-content-logo w-100 d-flex justify-content-center flex-wrap align-items-center"
           style={{ backgroundImage: `url(${logo})` }}
@@ -15,7 +15,7 @@ const Home = ({ nextStep, setKindUser }) => {
         <h1 className="content-logo-title text-primary">
           Controle de Ambientes Fisicos
         </h1>
-        <div className="home-content-options w-100 d-flex flex-grow-1 flex-wrap">
+        <div className="home-content-options d-flex">
           <Button
             onClick={() => {
               setKindUser("employee");
