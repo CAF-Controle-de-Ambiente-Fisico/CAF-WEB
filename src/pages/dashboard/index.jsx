b555d1c0093c6cdee5409ed33b6e90efb0254df7
edubcoelho <luizbcoelho27@outlook.com>
2021-04-09 10:19:04 -0300

    staged dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import logoHeader from "./../../assets/images/handonkey.svg";
 import userIcon from "./../../assets/images/icons/user-icon.svg";
-import copyIcon from "./../../assets/images/icons/copy.svg";
+// import copyIcon from "./../../assets/images/icons/copy.svg";
 
 function index() {
-  const copyCode = () => {
-    if (process.browser) {
-      let inputCode = document.getElementById("current-code");
-      inputCode.select();
-      inputCode.setSelectionRange(0, 99999);
+  // const copyCode = () => {
+  //  if (process.browser) {
+  //    let inputCode = document.getElementById("current-code");
+  //    inputCode.select();
+  //    inputCode.setSelectionRange(0, 99999);
 
-      document.execCommand("copy");
-    }
-  };
+  //    document.execCommand("copy");
+  //  }
+  // };
   return (
     <div className="dashboard-box d-flex justify-content-center align-items-center">
       <div className="dashboard-header d-flex justify-content-center align-items-center">
@@ -23,7 +23,7 @@ function index() {
         <div className="dashboard-header-app-name">CAF</div>
 
         <div className="dashboard-header-user-info mr-2 d-flex align-items-center">
-          <div>Bem vindo, fulano</div>
+          <div>Fulano</div>
           <div className="divider-y ml-2 mr-2"></div>
           <div className="h-100 d-flex justify-content-center align-items-center">
             <img src={userIcon} height="35px" />
@@ -36,14 +36,12 @@ function index() {
           <div className="text-code-title">Código ativo</div>
           <div className="divider-x mt-3 mb-3"></div>
           <div className="code-value ml-2 mr-2 d-flex justify-content-center align-items-center">
-            <button onClick={copyCode} className="copy-button mr-2">
+            {/* <button className="copy-button mr-2">
               <img height="25" src={copyIcon} alt="" />
-            </button>
-            <input
-              id="current-code"
-              className="code-value-text"
-              value="aa23dfe32aa23dfe32aa23dfe32aa23dfe32aa23dfe32aa23dfe32"
-            />
+            </button> */}
+            <div className="code-value-text">
+              aa23dfe32aa23dfe32aa23dfe32aa23dfe32aa23dfe32aa23dfe32"
+            </div>
           </div>
         </div>
       </div>
