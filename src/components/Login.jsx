50f5a38635fe73c68c70c2cd7513619fbe5ffabb
Harysson Soares <haryssonsoares@gmail.com>
2021-04-09 16:54:38 -0300

    feat: state to loading and logged

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { Button, Form, Image } from "react-bootstrap";
 import { useForm, FormProvider } from "react-hook-form";
 import { getSession, getCsrfToken, signIn } from "next-auth/client";
 import ReCAPTCHA from "react-google-recaptcha";
+import ReactLoading from "react-loading";
+import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
 
 import Input from "../components/Form/Input";
 
@@ -18,7 +21,13 @@ import { nextAuth } from "../../service/api";
 const Login = ({ firstStep, kindUser = "" }) => {
   const methods = useForm();
   const router = useRouter();
+
   const [reCaptcha, setReCaptcha] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [isLogged, setIsLogged] = useState(true);
+
+  const MySwal = withReactContent(Swal);
+  const { callbackUrl } = router.query;
 
   const handleRePCAPTCHA = () => setReCaptcha(true);
 
@@ -27,112 +36,150 @@ const Login = ({ firstStep, kindUser = "" }) => {
 
     if (reCaptcha) {
       setLoading(true);
+      console.log(data);
       nextAuth
         .post("api/auth/callback/domain-signin", {
           ...data,
           csrfToken,
-          callbackUrl: `${process.env.NEXT_PUBLIC_SITE_URL}/profile`,
+          callbackUrl: `${process.env.NEXT_PUBLIC_SITE_URL}/dashboard`,
         })
         .then((response) => {
-          if (response.request.responseURL.includes("Error")) {
+          console.log("response = ", response);
+          if (response.request.responseURL.includes("error")) {
             MySwal.fire({
               icon: "error",
               title: "Oops...",
               text: "Email ou senha incorreto(s)",
+            }).then(() => {
+              router.reload();
             });
-            setLoading(false);
           } else {
             MySwal.fire({
               icon: "success",
               title: "Tudo certo!",
               text: "Login realizado com sucesso",
             }).then(() => {
-              router.push(`${process.env.NEXT_PUBLIC_SITE_URL}/profile`);
+              router.push(`${process.env.NEXT_PUBLIC_SITE_URL}/dashboard`);
             });
-            setLoading(false);
           }
+          setLoading(false);
         })
         .catch((error) => {
+          console.log("ERROR FRONT = ", error);
           MySwal.fire({
             icon: "error",
             title: "Oops...",
             text: "Algo deu errado",
-          });
-          setLoading(false);
+          }).then(() => setLoading(false));
         });
     }
   };
 
+  useEffect(async () => {
+    const session = await getSession();
+    if (session) {
+      if (callbackUrl) {
+        router.push(callbackUrl);
+      } else {
+        MySwal.fire({
+          icon: "success",
+          title: "Sessão ativa",
+          text: "Você já fez login no sistema",
+        }).then(() => {
+          router.push(`${process.env.NEXT_PUBLIC_SITE_URL}/dashboard`);
+        });
+      }
+    } else setIsLogged(false);
+    setLoading(false);
+  }, []);
+
   return (
-    <div className="login d-flex ">
-      <div
-        className="login-bg"
-        style={{ backgroundImage: `url(${bgContet})` }}
-      ></div>
-      <div className="login-form h-100 d-flex flex-column align-items-center justify-content-center">
-        <div className="login-form-logo d-flex flex-wrap flex-column align-items-center w-100">
-          <Image src={logo} />
-          <p>
-            Faça seu login como{" "}
-            <strong className="text-uppercase">{translate(kindUser)}</strong>
-          </p>
+    <>
+      {loading ? (
+        <div className="d-flex justify-content-center align-items-center vh-100">
+          <ReactLoading
+            type="spinningBubbles"
+            color="#1d3557"
+            height={200}
+            width={200}
+          />
         </div>
-        <FormProvider {...methods}>
-          <Form
-            onSubmit={methods.handleSubmit(onSubmit)}
-            className="w-100 d-flex flex-column align-items-center flex-wrap"
-          >
-            <Input
-              name="email"
-              type="email"
-              placeholder="Usuário ou email"
-              label={<Image className="mb-1" src={user} />}
-              contextClassName="position-relative d-flex justify-content-center"
-              labelClassName="position-absolute login-form-label"
-              className="text-center login-form-input input-username"
-            />
-
-            <Input
-              name="password"
-              type="password"
-              placeholder="Senha"
-              label={<Image className="mb-1" src={password} />}
-              contextClassName="position-relative d-flex justify-content-center"
-              labelClassName="position-absolute login-form-label"
-              className="text-center login-form-input input-password"
-            />
-
-            <div className="d-flex justify-content-center mb-3">
-              <ReCAPTCHA
-                sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY}
-                onChange={handleRePCAPTCHA}
-              />
+      ) : (
+        <div className="login d-flex ">
+          <div
+            className="login-bg"
+            style={{ backgroundImage: `url(${bgContet})` }}
+          ></div>
+          <div className="login-form h-100 d-flex flex-column align-items-center justify-content-center">
+            <div className="login-form-logo d-flex flex-wrap flex-column align-items-center w-100">
+              <Image src={logo} />
+              <p>
+                Faça seu login como{" "}
+                <strong className="text-uppercase">
+                  {translate(kindUser)}
+                </strong>
+              </p>
             </div>
+            <FormProvider {...methods}>
+              <Form
+                onSubmit={methods.handleSubmit(onSubmit)}
+                className="w-100 d-flex flex-column align-items-center flex-wrap"
+              >
+                <Input
+                  name="email"
+                  type="email"
+                  placeholder="Usuário ou email"
+                  label={<Image className="mb-1" src={user} />}
+                  contextClassName="position-relative d-flex justify-content-center"
+                  labelClassName="position-absolute login-form-label"
+                  className="text-center login-form-input input-username"
+                />
 
-            <Button
-              className="login-form-button mt-3 badge badge-pill"
-              type="submit"
-            >
-              Login
-            </Button>
+                <Input
+                  name="password"
+                  type="password"
+                  placeholder="Senha"
+                  label={<Image className="mb-1" src={password} />}
+                  contextClassName="position-relative d-flex justify-content-center"
+                  labelClassName="position-absolute login-form-label"
+                  className="text-center login-form-input input-password"
+                />
+                <Form.Group>
+                  <div className="d-flex justify-content-center my-3">
+                    <ReCAPTCHA
+                      sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY}
+                      onChange={handleRePCAPTCHA}
+                    />
+                  </div>
+                </Form.Group>
 
-            <Button
-              variant="warning"
-              onClick={() => firstStep()}
-              className="login-form-button mt-3 badge badge-pill"
-            >
-              Voltar
-            </Button>
+                <Button
+                  className="login-form-button mt-3 badge badge-pill"
+                  type="submit"
+                  disabled={!reCaptcha}
+                >
+                  Login
+                </Button>
 
-            <Link href={`/signup/${kindUser}`}>
-              <a className="mt-3 text-center text-primary login-form-link">
-                Não possui conta? <br /> Clique aqui para cadastre-se
-              </a>
-            </Link>
-          </Form>
-        </FormProvider>
-      </div>
-    </div>
+                <Button
+                  variant="warning"
+                  onClick={() => firstStep()}
+                  className="login-form-button mt-3 badge badge-pill"
+                >
+                  Voltar
+                </Button>
+
+                <Link href={`/signup/${kindUser}`}>
+                  <a className="mt-3 text-center text-primary login-form-link">
+                    Não possui conta? <br /> Clique aqui para cadastre-se
+                  </a>
+                </Link>
+              </Form>
+            </FormProvider>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
